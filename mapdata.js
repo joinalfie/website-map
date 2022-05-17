bd1dd4a0fc2dba79bef8f2a85976f46e3aa7e520
ale-singh <98822191+ale-singh@users.noreply.github.com>
2022-05-17 14:37:17 -0400

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,441 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    hide_labels: "no",
+    border_color: "white",
+    state_description: "",
+    state_color: "#041e4b",
+    state_hover_color: "#f0a783",
+    state_url: "https://www.joinalfie.com/locations/coming-soon",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "#041e4b",
+    label_size: "",
+    label_font: "Inter",
+    popups: "detect"
+  },
+  state_specific: {
+    "0": {},
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/hawaii"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/alaska"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/florida"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/new-hampshire"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/vermont"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/maine"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/rhode-island"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/new-york"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/pennsylvania"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/new-jersey"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/delaware"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/maryland"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/virginia"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/west-virginia"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/ohio"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/indiana"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/illinois"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/connecticut"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/wisconsin"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/north-carolina"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/district-of-columbia"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/massachusetts"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/tennessee"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/arkansas"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/missouri"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/georgia"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/south-carolina"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/kentucky"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/alabama"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/louisiana"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/mississippi"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/iowa"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/minnesota"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/oklahoma"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/texas"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/new-mexico"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/kansas"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/nebraska"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/south-dakota"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/north-dakota"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/wyoming"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/montana"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/colorado"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/utah"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/arizona"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/nevada"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/oregon"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/washington"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/california"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/michigan"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/idaho"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/guam"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/virgin-islands"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/puerto-rico"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "yes",
+      zoomable: "no",
+      url: "https://www.joinalfie.com/locations/northern-mariana-islands"
+    }
+  },
+  locations: {},
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
